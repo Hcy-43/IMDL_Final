e264f795d06d4ba0049e9c7346f4cd865521be43
Hcy-43 <101809005+Hcy-43@users.noreply.github.com>
2025-05-23 17:38:00 +0800

    dataset

--- web_crawl/scripts/web_crawl.sh
+++ web_crawl/scripts/web_crawl.sh
@@ -1,5 +1,38 @@
-keyword="ちいかわ ハチワレ"
-save_folder="hachiware"
+keyword="ちいかわ ラッコ"
+save_folder="rakko"
+num_scrolls=100
+max_download=100
+python crawl.py \
+    --keyword "$keyword" \
+    --save_folder "$save_folder" \
+    --num_scrolls $num_scrolls \
+    --max_download $max_download
+
+keyword="ちいかわ くりまんじゅう"
+save_folder="kurimanjyu"
+num_scrolls=100
+max_download=100
+python crawl.py \
+    --keyword "$keyword" \
+    --save_folder "$save_folder" \
+    --num_scrolls $num_scrolls \
+    --max_download $max_download
+
+
+keyword="ちいかわ シーサー"
+save_folder="shiisaa"
+num_scrolls=100
+max_download=100
+python crawl.py \
+    --keyword "$keyword" \
+    --save_folder "$save_folder" \
+    --num_scrolls $num_scrolls \
+    --max_download $max_download
+
+
+
+keyword="ちいかわ 鎧さん"
+save_folder="yoroisan"
 num_scrolls=100
 max_download=100
 python crawl.py \
